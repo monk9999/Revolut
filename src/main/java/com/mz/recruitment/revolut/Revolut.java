f2f1928b452e0fe2b3c8b38ad5b31b77cd70e0fb
Michał Zimnicki <michal.zimnicki@tomtom.com>
2018-06-09 23:16:00 +0200

    add new account

--- src/main/java/com/mz/recruitment/revolut/Revolut.java
+++ src/main/java/com/mz/recruitment/revolut/Revolut.java
@@ -30,6 +30,7 @@ public final class Revolut {
         accountRepository.save(AccountFactory.getAccountFactory().createEmptyAccount("03"));
         accountRepository.save(AccountFactory.getAccountFactory().createEmptyAccount("04"));
         accountRepository.save(AccountFactory.getAccountFactory().createEmptyAccount("05"));
+		accountRepository.save(AccountFactory.getAccountFactory().createEmptyAccount("06"));
         accountRepository.save(AccountFactory.getAccountFactory().createAccountWithBalance("1",
                 BigDecimal.valueOf(100000)));
         accountRepository.save(AccountFactory.getAccountFactory().createAccountWithBalance("2",
@@ -40,6 +41,8 @@ public final class Revolut {
                 BigDecimal.valueOf(400000)));
         accountRepository.save(AccountFactory.getAccountFactory().createAccountWithBalance("5",
                 BigDecimal.valueOf(500000)));
+		accountRepository.save(AccountFactory.getAccountFactory().createAccountWithBalance("6",
+                BigDecimal.valueOf(600000)));
     }
 }
 
